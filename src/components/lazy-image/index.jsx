da74cf3f3e73e930660163ea84afcbbd64ed8c3b
TML17 <60923213+TML17@users.noreply.github.com>
2023-12-12 16:59:45 -0600

    update

--- src/components/lazy-image/index.jsx
+++ src/components/lazy-image/index.jsx
@@ -1,10 +1,35 @@
 
-import React from 'react';
+renderExternalProjects = () => {
+    return externalProjects.map((item, index) => (
+      <div className="project-container" key={index}>
+        {/* Description at the top */}
+        <div className="project-description">
+          <h2>{item.title}</h2> {/* Assuming 'item.title' is the title of the project */}
+          <p>{item.description}</p> {/* Assuming 'item.description' is the description */}
+        </div>
 
-// Example implementation of a LazyImage component as a functional component
-const LazyImage = ({ src, alt, className }) => {
-  return <img src={src} alt={alt} className={className} />;
+        {/* Image below the description */}
+        <a
+          className="project-image"
+          href={item.link}
+          onClick={(e) => {
+            e.preventDefault();
+            if (googleAnalytics?.id) {
+              ga.event({
+                action: 'Click External Project',
+                params: {
+                  post: item.title,
+                },
+              });
+            }
+          }}
+        >
+          <LazyImage
+            src={item.imageUrl}
+            alt={item.title}
+            className="w-full"
+          />
+        </a>
+      </div>
+    ));
 };
-
-// Default export of the LazyImage component
-export default LazyImage;
